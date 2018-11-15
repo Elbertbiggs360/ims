50f58febf33c46c881de1aecd28192dd25b0bb20
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-11-15 12:53:14 +0300

    Turn on debugging for sample application

--- hello_world.py
+++ hello_world.py
@@ -6,12 +6,12 @@ class BaseHandler(RequestHandler):
 
 class MainHandler(BaseHandler):
   def get(self):
-    self.write({'message': 'Hello, World'})
+    self.write({'message': 'API is running...'})
 
 def make_app():
   return Application([
     (r"/", MainHandler)
-  ])
+  ], debug=True)
 
 if __name__ == "__main__":
     app = make_app()
