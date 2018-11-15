29b0859187aff7bafe1bfaa05e916a3a3f05ae22
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-11-15 19:16:11 +0300

    add request handler methods for all methods

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import tornado.ioloop
 from tornado.web import Application, RequestHandler
 
+companies=addresses=people=files=departments=contract = []
+
 class BaseHandler(RequestHandler):
   pass
 
@@ -8,9 +10,100 @@ class MainHandler(BaseHandler):
   def get(self):
     self.write('API is running at port 8888...')
 
+class Companies(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Addresses(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class People(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Files(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Departments(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Contacts(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Employees(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Spectrum(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class TypeApproval(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Numbering(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Broadcasting(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
+class Postal(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+    
+class Telecom(BaseHandler):
+  def get(self):
+    pass
+  def post(self):
+    pass
+
 def make_app():
   return Application([
-    (r"/", MainHandler)
+    (r"/", MainHandler),
+    ('/companies', Companies),
+    ('/addresses', Addresses),
+    ('/people', People),
+    ('/files', Files),
+    ('/departments', Departments),
+    ('/contacts', Contacts),
+    ('/employees', Employees),
+    ('/spectrum', Employees),
+    ('/typeapproval', TypeApproval),
+    ('/numbering', Numbering),
+    ('/broadcasting', Broadcasting),
+    ('/postal', Postal),
+    ('/telecom', Telecom)
   ], debug=True)
 
 if __name__ == "__main__":
